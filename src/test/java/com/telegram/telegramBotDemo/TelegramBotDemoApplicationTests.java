89d913b4524701024a53461267abb5e0489d9496
Sergei-Bashinskii <sergei13121390@gmail.com>
2024-07-01 22:59:54 +0300

    v0.0

---
+++ src/test/java/com/telegram/telegramBotDemo/TelegramBotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegram.telegramBotDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
